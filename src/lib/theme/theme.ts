a041f30a883619eb7fcea236575620fa0585a44f
Mostafa Milly <57003058+MostafaKMilly@users.noreply.github.com>
2024-09-07 12:34:49 +0300

    Make font sizes responsive

--- src/lib/theme/theme.ts
+++ src/lib/theme/theme.ts
@@ -1,15 +1,17 @@
 "use client";
 
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 import typography from "./typography";
 import { componentsOverrides } from "./overrides";
 import { palette } from "./palette";
 import { shadows } from "./shadows";
 
-export const theme = createTheme({
-  palette,
-  typography,
-  shadows,
-  components: componentsOverrides,
-  direction: "ltr",
-});
+export const theme = responsiveFontSizes(
+  createTheme({
+    palette,
+    typography,
+    shadows,
+    components: componentsOverrides,
+    direction: "ltr",
+  })
+);
